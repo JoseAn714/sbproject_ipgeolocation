da0ef22126f57123468f64b55b16f10aa23b806f
JaLizarragaU <joanlu00@gmail.com>
2022-12-11 12:35:13 -0600

    Primera edicion

--- src/main/java/com/springbootproject/ipgeolocation/IpgeolocationApplication.java
+++ src/main/java/com/springbootproject/ipgeolocation/IpgeolocationApplication.java
@@ -3,11 +3,18 @@ package com.springbootproject.ipgeolocation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// import java.io.IOException;
+// import com.springbootproject.ipgeolocation.service.LocationService;
+
 @SpringBootApplication
 public class IpgeolocationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IpgeolocationApplication.class, args);
 	}
+
+	// public static void main(String[] args) throws IOException {
+	// 	System.out.println(LocationService.GenerateUrl().get("url"));
+	// }
 
 }
---
+++ src/main/java/com/springbootproject/ipgeolocation/controller/WebController.java
@@ -0,0 +1,40 @@
+package com.springbootproject.ipgeolocation.controller;
+
+import java.io.IOException;
+
+import org.json.JSONException;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.springbootproject.ipgeolocation.service.LocationService;
+
+@Controller
+public class WebController {
+
+	
+    @RequestMapping("/")
+	public String showMap(Model model) throws JSONException, IOException {
+		
+		// esta es la vista de retorno que necesitamos
+        model.addAttribute("ip",LocationService.GetIp().getString("ip"));
+        model.addAttribute("loc", LocationService.GetIpInfo().getString("loc"));
+        model.addAttribute("lat", LocationService.GetIpInfo().getString("loc").split(",")[0]);
+        model.addAttribute("lon", LocationService.GetIpInfo().getString("loc").split(",")[1]);
+        model.addAttribute("url", LocationService.GenerateUrl().get("url"));
+		return "map";
+		
+	}
+
+    // @GetMapping("/greeting") //Esto solo indica es para acceder a la siguiente función solo con operación tipo get
+    // public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) { 
+    //     Persona p = new Persona();
+    //     p.setIdPersona(1);
+    //     p.setNombre("Jose");
+    //     repo.save(p);
+        
+    //     model.addAttribute("name", name);
+    //     return "greeting"; 
+    // }
+
+}

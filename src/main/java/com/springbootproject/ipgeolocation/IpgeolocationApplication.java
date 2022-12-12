0a028f0dd9d129c3c45d73002af96cb1b6ced22f
JaLizarragaU <joanlu00@gmail.com>
2022-12-12 09:58:38 -0600

    Final_v3

--- src/main/java/com/springbootproject/ipgeolocation/IpgeolocationApplication.java
+++ src/main/java/com/springbootproject/ipgeolocation/IpgeolocationApplication.java
@@ -3,18 +3,11 @@ package com.springbootproject.ipgeolocation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// import java.io.IOException;
-// import com.springbootproject.ipgeolocation.service.LocationService;
-
 @SpringBootApplication
 public class IpgeolocationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IpgeolocationApplication.class, args);
 	}
-
-	// public static void main(String[] args) throws IOException {
-	// 	System.out.println(LocationService.GenerateUrl().getString("url"));
-	// }
 
 }
